763183971bf9074d22f795aebf06feffa3b6a32a
Alf Henderson <alfhenderson@gmail.com>
2018-09-13 14:50:05 +0200

    WILL-894/BUGFIX: Scheduled post redirects

--- src/Actions/Backend/PostSlugChange.php
+++ src/Actions/Backend/PostSlugChange.php
@@ -5,6 +5,7 @@ namespace Bonnier\Willow\Base\Actions\Backend;
 use Bonnier\Willow\MuPlugins\Helpers\LanguageProvider;
 use Bonnier\WP\ContentHub\Editor\Models\WpComposite;
 use Bonnier\WP\Redirect\Http\BonnierRedirect;
+
 /**
  * Class PostSlugChange
  *
@@ -17,14 +18,17 @@ class PostSlugChange
         add_filter(WpComposite::SLUG_CHANGE_HOOK, [$this, 'createRedirectOnSlugChange'], 10, 3);
     }
 
-    public function createRedirectOnSlugChange($postId, $oldLink, $newLink) {
-        BonnierRedirect::createRedirect(
-            parse_url($oldLink, PHP_URL_PATH),
-            parse_url($newLink, PHP_URL_PATH),
-            LanguageProvider::getCurrentLanguage(),
-            'post-slug-change',
-            $postId
-        );
+    public function createRedirectOnSlugChange($postId, $oldLink, $newLink)
+    {
+        if (get_post_status($postId) === 'publish') {
+            BonnierRedirect::createRedirect(
+                parse_url($oldLink, PHP_URL_PATH),
+                parse_url($newLink, PHP_URL_PATH),
+                LanguageProvider::getCurrentLanguage(),
+                'post-slug-change',
+                $postId
+            );
+        }
     }
 }
 
