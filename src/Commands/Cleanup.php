3e987a434bdee3585fac7734d3ee631b300a4b7d
Magnus Flor Johansen <magnus@m-flor.dk>
2018-10-16 14:12:29 +0200

    IVD-423 - Updating delete script The delete script can now handle pages, posts, categories, tags and all respective translations of said content.

--- src/Commands/Cleanup.php
+++ src/Commands/Cleanup.php
@@ -8,22 +8,25 @@ use Bonnier\WP\Redirect\Http\BonnierRedirect;
 use Illuminate\Support\Collection;
 use League\Csv\Exception;
 use League\Csv\Reader;
-use League\Csv\Statement;
-use WP_CLI;
-use WP_CLI_Command;
-use WP_Post;
 
-class Cleanup extends WP_CLI_Command
+class Cleanup extends \WP_CLI_Command
 {
     const CMD_NAMESPACE = 'cleanup';
 
+    protected $resolveCache;
+    protected $content;
+
     public static function register()
     {
-        WP_CLI::add_command(sprintf(
-            '%s %s',
-            CommandBootstrap::CORE_CMD_NAMESPACE,
-            self::CMD_NAMESPACE
-        ), __CLASS__);
+        try {
+            \WP_CLI::add_command(sprintf(
+                '%s %s',
+                CommandBootstrap::CORE_CMD_NAMESPACE,
+                self::CMD_NAMESPACE
+            ), __CLASS__);
+        } catch (\Exception $exception) {
+            \WP_CLI::warning($exception);
+        }
     }
 
     /**
@@ -44,32 +47,45 @@ class Cleanup extends WP_CLI_Command
      * ---
      *
      * ## EXAMPLES
-     *     wp contenthub editor cleanup delete list.csv --delimiter=';'
+     *     wp willow cleanup delete list.csv --delimiter=';'
      *
      * @param $args
+     * @param $assocArgs
      *
-     * @throws WP_CLI\ExitException
+     * @throws \WP_CLI\ExitException
      */
-    public function delete($args, $assoc_args)
+    public function delete($args, $assocArgs)
     {
         list($file) = $args;
-        $delimiter = $assoc_args['delimiter'] ?: ',';
+        $delimiter = $assocArgs['delimiter'] ?: ',';
         if (!file_exists($file)) {
             $file = dirname(dirname(ABSPATH)) . $file;
             if (!file_exists($file)) {
-                WP_CLI::error('File not found');
+                \WP_CLI::error('File not found');
             }
         }
 
-        $urls = $this->processCSV($file, $delimiter);
+        $this->processCSV($file, $delimiter);
 
-        $articles = $this->processURLs($urls);
-
-        $this->handleArticles($articles);
-
-        WP_CLI::success('Done!');
+        \WP_CLI::success('Done!');
     }
 
+    /**
+     * Converts a CSV of 'from' and 'to' urls to a collection with associative arrays
+     * [
+     *    'from' => '/category/artice',
+     *    'fromContent' => WP_Post{}
+     *    'to' => '/category'
+     *    'toContent' => WP_Term{}
+     * ]
+     *
+     * @param string $file
+     * @param string $delimiter
+     *
+     * @return Collection
+     *
+     * @throws \WP_CLI\ExitException
+     */
     private function processCSV(string $file, string $delimiter): Collection
     {
         $urls = new Collection();
@@ -79,111 +95,258 @@ class Cleanup extends WP_CLI_Command
             $csv->setDelimiter($delimiter);
             $csv->setHeaderOffset(0);
         } catch (Exception $exception) {
-            WP_CLI::error($exception->getMessage());
+            \WP_CLI::error($exception->getMessage());
         }
 
-        foreach ($csv as $line => $record) {
-            $fromUrl = $record['from'];
-            $toUrl = $record['to'];
-            if (!filter_var($fromUrl, FILTER_VALIDATE_URL)) {
-                WP_CLI::error('Invalid input on line ' . ($line + 1));
+        $this->resolveCache = [];
+        $progress = \WP_CLI\Utils\make_progress_bar(
+            'Deleting content',
+            count($csv)
+        );
+        $this->content = [];
+        foreach ($csv as $record) {
+            $progress->tick();
+
+            // Get the content that the from column matches
+            // Either WP_Post or WP_Term (Pages, composites, categories or tags)
+            $fromContent = $this->resolveContent($record['from'] ?? null);
+            if (!$fromContent) {
+                continue;
             }
-            $urls->put($fromUrl, $toUrl);
+            // Get the content that the to column matches
+            // Either WP_Post or WP_Term (Pages, composites, categories or tags)
+            $toContent = $this->resolveContent($record['to'] ?? null);
+
+            // Get all translations for the content
+            // and push the danish content into the collection
+            $fromTranslations = $this->getTranslations($fromContent);
+            $fromTranslations->put('da', $fromContent);
+            $toTranslations = $this->getTranslations($toContent);
+            $toTranslations->put('da', $toContent);
+
+            // Foreach from content on all languages, handle the content
+            // and match it to the destination (to) content.
+            $fromTranslations->each(function ($content, $locale) use ($toTranslations) {
+                $this->handleContent($content, $toTranslations->get($locale), $locale);
+            });
         }
+        $progress->finish();
 
-        WP_CLI::line(sprintf('Found %s urls to be deleted%s', $urls->count(), PHP_EOL));
-
+        \WP_CLI::line(sprintf('Processed %s entries', $urls->count()));
+        foreach ($this->content as $locale => $count) {
+            \WP_CLI::line(sprintf('Handled %s %s contents', $count, $locale));
+        }
         return $urls;
     }
 
-    private function processURLs(Collection $urls): Collection
+    /**
+     * Converting a relative url to its corresponding content.
+     *
+     * @param string $url
+     * @return array|bool|null|\WP_Post|\WP_Term
+     */
+    private function resolveContent(string $url)
     {
-        $hostLanguage = collect(LanguageProvider::getLanguageList())->mapWithKeys(function ($language) {
-            if (($homeUrl = $language->home_url ?? null) && ($slug = $language->slug ?? null)) {
-                return [parse_url($homeUrl, PHP_URL_HOST) => $slug];
+        if (($content = $this->resolveCache[$url] ?? false) && $content !== false) {
+            return $content;
+        }
+
+        if (null === $url || '/' === $url) {
+            $frontpage = get_post(get_option('page_on_front'));
+            $this->resolveCache[$url] = $frontpage;
+            return $frontpage;
+        }
+
+        if (preg_match('#/?tags/([^/]+)$#', $url, $match)) {
+            $slug = $match[1];
+            if ($tag = get_term_by('slug', $slug, 'post_tag')) {
+                $this->resolveCache[$url] = $tag;
+                return $tag;
             }
-            return null;
-        })->reject(function ($language) {
-            return is_null($language);
-        });
-        return $urls->map(function (string $toUrl, $fromUrl) use ($hostLanguage) {
-            $host = parse_url($fromUrl, PHP_URL_HOST);
-            $path = parse_url($fromUrl, PHP_URL_PATH);
-            return [
-                'from' => $fromUrl,
-                'to' => $toUrl,
-                'slug' => $hostLanguage->get($host, 'da'),
-                'post' => with(new RouteController)->findContenthubComposite($path, 'all'),
-            ];
-        });
+        }
+
+        if (($category = get_category_by_path($url)) && $category instanceof \WP_Term) {
+            $this->resolveCache[$url] = $category;
+            return $category;
+        }
+
+        if (($page = get_page_by_path($url)) && $page instanceof \WP_Post) {
+            $this->resolveCache[$url] = $page;
+            return $page;
+        }
+
+        if ($composite = with(new RouteController)->findContenthubComposite($url, 'all')) {
+            $this->resolveCache[$url] = $composite;
+            return $composite;
+        }
+
+        $this->resolveCache[$url] = null;
+        return null;
     }
 
-    private function handleArticles(Collection $articles)
+    /**
+     * Create a redirect and delete the "from"-content
+     *
+     * @param \WP_Post|\WP_Term|null $fromContent
+     * @param \WP_Post|\WP_Term|null$toContent
+     * @param string $locale
+     */
+    private function handleContent($fromContent, $toContent, $locale)
     {
-        $articles->each(function (array $article) {
-            WP_CLI::line(sprintf('Deleting "%s"...', $article['from']));
-            if ($post = $article['post']) {
-                wp_delete_post($post->ID, $forceDelete = true);
-                WP_CLI::line(sprintf('Deleted post with id %s', $post->ID));
-            } else {
-                WP_CLI::line('No Composite to delete');
-            }
-            $this->handleRedirect($article);
-            WP_CLI::line('Deletion done!' . PHP_EOL);
-        });
-    }
+        // If we cannot find a url, or the url is the frontpage
+        // skip
+        if ((!$fromUrl = $this->getUrl($fromContent)) || $fromUrl === '/') {
+            return;
+        }
 
-    private function handleRedirect(array $article)
-    {
-        $toUrl = null;
-        if (filter_var($article['to'], FILTER_VALIDATE_URL)) {
-            WP_CLI::line(sprintf('Creating redirect from "%s" to "%s"...', $article['from'], $article['to']));
-            $toUrl = $article['to'];
+        // If we cannot find a destination URL, we'll redirect to the frontpage
+        if (!$toUrl = $this->getDestinationUrl($toContent, $fromContent, $fromUrl)) {
+            $toUrl = '/';
+        }
+
+        // If from and to are the same
+        // skip
+        if ($fromUrl === $toUrl) {
+            return;
+        }
+
+        // Create a redirect through the BonnierRedirect plugin
+        BonnierRedirect::handleRedirect(
+            $fromUrl,
+            $toUrl,
+            $locale,
+            'cleanup-delete-script',
+            $fromContent->ID ?? $fromContent->term_id ?? 0,
+            301,
+            true
+        );
+
+        $this->deleteContent($fromContent);
+
+        // Just to have an idea of how much content was handled
+        if (!isset($this->content[$locale])) {
+            $this->content[$locale] = 1;
         } else {
-            WP_CLI::line('No destination URL.');
-            WP_CLI::line('Creating redirect to parent category...');
-            $toUrl = $this->getParentCategory($article);
-            WP_CLI::line(sprintf('Creating redirect from "%s" to "%s"...', $article['from'], $toUrl));
-        }
-
-        if ($toUrl) {
-            if (parse_url($article['from'], PHP_URL_PATH) == (parse_url($toUrl, PHP_URL_PATH) ?: '/')) {
-                WP_CLI::warning('Cannot create redirect when "from" and "to" are identical!');
-                return;
-            }
-            if (BonnierRedirect::handleRedirect(
-                parse_url($article['from'], PHP_URL_PATH),
-                parse_url($toUrl, PHP_URL_PATH) ?: '/',
-                $article['slug'],
-                'cleanup-delete-script',
-                $article['post']->ID ?? 0
-            ) === true) {
-                WP_CLI::line('Redirect created');
-            } else {
-                WP_CLI::warning('Failed creating redirect');
-            }
-        } else {
-            WP_CLI::warning('Unable to create redirect');
+            $this->content[$locale]++;
         }
     }
 
-    private function getParentCategory(array $article): string
+    /**
+     * Convert the content to a url-path
+     *
+     * @param \WP_Post|\WP_Term|null $content
+     * @return string|null
+     */
+    private function getUrl($content)
     {
-        if (($post = $article['post']) && $post instanceof WP_Post) {
-            list($categoryId) = wp_get_post_categories($post->ID);
-            if ($categoryUrl = get_category_link($categoryId)) {
-                return $categoryUrl;
-            }
+        if ($content instanceof \WP_Post) {
+            return parse_url(get_permalink($content->ID), PHP_URL_PATH);
+        } elseif ($content instanceof \WP_Term && $content->taxonomy === 'category') {
+            return parse_url(get_category_link($content->term_id), PHP_URL_PATH);
+        } elseif ($content instanceof \WP_Term) {
+            return parse_url(get_term_link($content->term_id), PHP_URL_PATH);
         }
 
-        if (preg_match('#^(.*)/.*$#', $article['from'], $matches)) {
+        return null;
+    }
+
+    /**
+     * Get the destination URL of the content
+     * Added fallbacks if the content doesn't resolve in a path.
+     *
+     * @param \WP_Post|\WP_Term|null $content
+     * @param \WP_Post|\WP_Term|null $fromContent
+     * @param string $fallbackUrl
+     * @return string|null
+     */
+    private function getDestinationUrl($content, $fromContent, $fallbackUrl)
+    {
+        // If the content has a URL, let's stick with that
+        if ($url = $this->getUrl($content)) {
+            return $url;
+        }
+
+        // If the fromContent has a parent category
+        // let's redirect to that.
+        if ($url = $this->getParentCategory($fromContent)) {
+            return $url;
+        }
+
+        // If we have a fallback url, that's based on the "from"-url,
+        // let's redirect to it's parent slug.
+        if (preg_match('#^(.*)/.*$#', $fallbackUrl, $matches)) {
             // Get everything before the last dash.
             // Given our URL structure, the parent category should be
             // on the URL, that excludes the slug of the post, which should
             // be after the last dash.
-            return $matches[1];
+            return parse_url($matches[1], PHP_URL_PATH);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the path of the parent category of a
+     * Page, post or category.
+     * Default to frontpage if no matches are found
+     *
+     * @param \WP_Post|\WP_Term|null $content
+     * @return string
+     */
+    private function getParentCategory($content)
+    {
+        if ($content instanceof \WP_Post) {
+            if ($categories = wp_get_post_categories($content->ID)) {
+                list($categoryId) = $categories;
+                if ($categoryUrl = get_category_link($categoryId)) {
+                    return parse_url($categoryUrl, PHP_URL_PATH);
+                }
+            }
+        } elseif ($content instanceof \WP_Term && $content->taxonomy === 'category' && $parent = $content->parent) {
+            if ($categoryUrl = get_category_link($parent)) {
+                return parse_url($categoryUrl, PHP_URL_PATH);
+            }
         }
 
         return '/';
+    }
+
+    /**
+     * Delete the content
+     *
+     * @param \WP_Post|\WP_Term|null $content
+     */
+    private function deleteContent($content)
+    {
+        if ($content instanceof \WP_Post) {
+            wp_delete_post($content->ID);
+        } elseif ($content instanceof \WP_Term) {
+            wp_delete_term($content->term_id, $content->taxonomy);
+        }
+    }
+
+    /**
+     * Get all translations for the content.
+     *
+     * @param \WP_Post|\WP_Term|null $content
+     * @return Collection
+     */
+    private function getTranslations($content): Collection
+    {
+        if (!$content) {
+            return new Collection();
+        }
+        if ($content instanceof \WP_Post) {
+            return collect(LanguageProvider::getPostTranslations($content->ID))->mapWithKeys(function ($id, $lang) {
+                return [$lang => get_post($id)];
+            });
+        } elseif ($content instanceof \WP_Term) {
+            return collect(LanguageProvider::getTermTranslations($content->term_id))
+                ->mapWithKeys(function ($id, $lang) {
+                    return [$lang => get_term($id)];
+                });
+        }
+
+        return new Collection();
     }
 }
