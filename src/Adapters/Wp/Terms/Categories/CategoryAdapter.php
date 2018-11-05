1b68326868abc300f3b3938783934c05877d879a
Magnus Flor Johansen <magnus@m-flor.dk>
2018-11-05 15:45:17 +0100

    Fix for tests

--- src/Adapters/Wp/Terms/Categories/CategoryAdapter.php
+++ src/Adapters/Wp/Terms/Categories/CategoryAdapter.php
@@ -56,8 +56,10 @@ class CategoryAdapter extends AbstractWpAdapter implements CategoryContract
     {
         parent::__construct($wpModel);
         $this->meta = $this->getMeta();
-        $this->acfFields = get_fields(sprintf('%s_%s', $this->wpModel->taxonomy, $this->wpModel->term_id));
-        $this->categoryContents = array_get($this->acfFields, AcfName::GROUP_PAGE_WIDGETS) ?: null;
+        if ($this->wpModel) {
+            $this->acfFields = get_fields(sprintf('%s_%s', $this->wpModel->taxonomy, $this->wpModel->term_id));
+            $this->categoryContents = array_get($this->acfFields, AcfName::GROUP_PAGE_WIDGETS) ?: null;
+        }
     }
 
     public function getId(): ?int
