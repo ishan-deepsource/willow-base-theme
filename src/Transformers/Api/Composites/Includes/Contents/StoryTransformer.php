ced6896e226d76aef28b6fa7871c08400cf77d57
Magnus Flor Johansen <magnus@m-flor.dk>
2018-11-06 12:03:43 +0100

    Bugfix - Force return articles as array instead of object

--- src/Transformers/Api/Composites/Includes/Contents/StoryTransformer.php
+++ src/Transformers/Api/Composites/Includes/Contents/StoryTransformer.php
@@ -19,7 +19,11 @@ class StoryTransformer extends TransformerAbstract
 
     private function transformTeaser(StoryContract $story)
     {
-        return with(new CompositeTeaserTransformer)->transform($story->getTeaser());
+        if ($teaser = $story->getTeaser()) {
+            return with(new CompositeTeaserTransformer)->transform($teaser);
+        }
+
+        return null;
     }
 
     private function transformArticles(StoryContract $story)
@@ -27,7 +31,7 @@ class StoryTransformer extends TransformerAbstract
         if ($articles = $story->getArticles()) {
             return $articles->map(function (CompositeContract $composite) {
                 return with(new CompositeTeaserTransformer)->transform($composite);
-            });
+            })->values()->toArray();
         }
 
         return null;
