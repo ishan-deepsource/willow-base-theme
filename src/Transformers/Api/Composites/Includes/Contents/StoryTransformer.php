fc188f83210e09cc67659c596bbc3cde25ecf72c
Magnus Flor Johansen <magnus@m-flor.dk>
2018-11-06 12:11:56 +0100

    Including instead of transforming

--- src/Transformers/Api/Composites/Includes/Contents/StoryTransformer.php
+++ src/Transformers/Api/Composites/Includes/Contents/StoryTransformer.php
@@ -2,38 +2,34 @@
 
 namespace Bonnier\Willow\Base\Transformers\Api\Composites\Includes\Contents;
 
-use Bonnier\Willow\Base\Models\Contracts\Composites\CompositeContract;
 use Bonnier\Willow\Base\Models\Contracts\Composites\Contents\StoryContract;
 use Bonnier\Willow\Base\Transformers\Api\Composites\CompositeTeaserTransformer;
 use League\Fractal\TransformerAbstract;
 
 class StoryTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = [
+        'teaser',
+        'articles',
+    ];
+
+    protected $defaultIncludes = [
+        'teaser',
+        'articles',
+    ];
+
     public function transform(StoryContract $story)
     {
-        return [
-            'teaser' => $this->transformTeaser($story),
-            'articles' => $this->transformArticles($story),
-        ];
+        return [];
     }
 
-    private function transformTeaser(StoryContract $story)
+    public function includeTeaser(StoryContract $story)
     {
-        if ($teaser = $story->getTeaser()) {
-            return with(new CompositeTeaserTransformer)->transform($teaser);
-        }
-
-        return null;
+        return $this->item($story->getTeaser(), new CompositeTeaserTransformer);
     }
 
-    private function transformArticles(StoryContract $story)
+    public function includeArticles(StoryContract $story)
     {
-        if ($articles = $story->getArticles()) {
-            return $articles->map(function (CompositeContract $composite) {
-                return with(new CompositeTeaserTransformer)->transform($composite);
-            })->values()->toArray();
-        }
-
-        return null;
+        return $this->collection($story->getArticles(), new CompositeTeaserTransformer);
     }
 }
