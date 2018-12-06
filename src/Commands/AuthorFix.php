2ef5cae891d0089a87dc0971bff8aa20c5155c9e
Frederik Rabøl <frederik-rm@hotmail.com>
2018-12-06 11:19:05 +0100

    added author clean up WP CLI command

---
+++ src/Commands/AuthorFix.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Bonnier\Willow\Base\Commands;
+
+use Bonnier\WP\ContentHub\Editor\Commands\CmdManager;
+use Bonnier\WP\ContentHub\Editor\Models\WpComposite;
+use WP_CLI;
+use WP_CLI_Command;
+
+class AuthorFix extends WP_CLI_Command
+{
+    const CMD_NAMESPACE = 'author';
+
+    public static function register()
+    {
+        WP_CLI::add_command(CmdManager::CORE_CMD_NAMESPACE  . ' ' . static::CMD_NAMESPACE, __CLASS__);
+    }
+
+    /**
+     * Prints a greeting.
+     *
+     * ## OPTIONS
+     *
+     * <locale>
+     * : The locale you want to use to find composites.
+     * <author>
+     * : The id of the user you want to set all composites to.
+     * ---
+     *
+     * ## EXAMPLES
+     *
+     *     wp contenteditor author fix da 3
+     *
+     */
+    public function fix($args)
+    {
+        if (count($args) < 2) {
+            WP_CLI::error('please provide both locale and author example:');
+            WP_CLI::error(sprintf('%s author fix da 3', CmdManager::CORE_CMD_NAMESPACE));
+        }
+
+        $locale = $args[0];
+        $author = $args[1];
+
+        $posts = get_posts([
+            'lang' => $locale,
+            'post_type' => WpComposite::POST_TYPE,
+            'numberposts' => -1,
+            'author' => 1,
+        ]);
+
+        $amountOfPosts = count($posts);
+
+        WP_CLI::line(sprintf('found %s composites with locale: %s', $amountOfPosts, $locale));
+
+        if ($amountOfPosts > 0) {
+            foreach ($posts as $post) {
+                WP_CLI::runcommand(sprintf('post update %d --post_author=%d', $post->ID, $author));
+            }
+        }
+    }
+
+    public function fixAll()
+    {
+        $users = [
+            'da' => 'Redaktionen',
+            'sv' => 'Redaktionen',
+            'nb' => 'Redaksjonen',
+            'fi' => 'Toimitus',
+            'nl' => 'de redactie',
+
+        ];
+        foreach ($users as $lang => $username) {
+            $user = get_user_by('login', $username);
+            if (empty($user)) {
+                return;
+            }
+            WP_CLI::line(sprintf('starting author fix for %s (%s)', $user->display_name, strtoupper($lang)));
+            WP_CLI::runcommand(sprintf(
+                '%s fix %s %d',
+                CmdManager::CORE_CMD_NAMESPACE  . ' ' . static::CMD_NAMESPACE,
+                $lang,
+                $user->ID
+            ));
+        }
+    }
+
+}
