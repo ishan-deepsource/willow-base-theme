f2f592c959b02db433acf81c02b0d988ec228e0b
Frederik Rabøl <frederik-rm@hotmail.com>
2019-04-26 10:25:16 +0200

    fixed getKind code that didnt make sense (too much vacaction)

--- src/Controllers/App/RouteController.php
+++ src/Controllers/App/RouteController.php
@@ -68,7 +68,7 @@ class RouteController extends BaseController
             $composite = new Composite(new CompositeAdapter($content));
             $resource = new Item($composite, new CompositeTransformer());
             $resource->setMeta([
-                'type' => $composite->getKind() == 'composite' ? $composite->getKind() : $composite->getKind(),
+                'type' => $composite->getKind(),
                 'status' => 200
             ]);
         } elseif ($content instanceof WP_Post && $content->post_type === 'page') {
