0ab30ad8c079962eb39807c123634fd2e4078af4
Magnus Flor <magnus@m-flor.dk>
2020-01-28 09:20:47 +0100

    WILL-1573 - Stripping subdomains from sitemap permalinks Updating WP Bonnier Sitemap to 1.2.0 to take advantage of new filters for permalinks being saved.

--- src/Actions/Universal/SitemapFilters.php
+++ src/Actions/Universal/SitemapFilters.php
@@ -3,6 +3,7 @@
 namespace Bonnier\Willow\Base\Actions\Universal;
 
 use Bonnier\WP\Sitemap\WpBonnierSitemap;
+use Illuminate\Support\Str;
 
 class SitemapFilters
 {
@@ -12,18 +13,21 @@ class SitemapFilters
 
     public function __construct()
     {
-        add_filter(WpBonnierSitemap::FILTER_ALLOWED_POST_TYPES, [$this, 'allowedPostTypes']);
-        add_filter(WpBonnierSitemap::FILTER_POST_ALLOWED_IN_SITEMAP, [$this, 'allowedInSitemap'], 10, 2);
+        add_filter(WpBonnierSitemap::FILTER_ALLOWED_POST_TYPES, [__CLASS__, 'allowedPostTypes']);
+        add_filter(WpBonnierSitemap::FILTER_POST_ALLOWED_IN_SITEMAP, [__CLASS__, 'allowedInSitemap'], 10, 2);
+        add_filter(WpBonnierSitemap::FILTER_POST_PERMALINK, [__CLASS__, 'removeApiSubdomain'], 10);
+        add_filter(WpBonnierSitemap::FILTER_CATEGORY_PERMALINK, [__CLASS__, 'removeApiSubdomain'], 10);
+        add_filter(WpBonnierSitemap::FILTER_TAG_PERMALINK, [__CLASS__, 'removeApiSubdomain'], 10);
     }
 
-    public function allowedPostTypes(array $postTypes)
+    public static function allowedPostTypes(array $postTypes)
     {
-        return array_filter($postTypes, function (string $postType) {
+        return array_values(array_filter($postTypes, function (string $postType) {
             return !in_array($postType, static::DISALLOWED_POST_TYPES);
-        });
+        }));
     }
 
-    public function allowedInSitemap(bool $allowed, \WP_Post $post)
+    public static function allowedInSitemap(bool $allowed, \WP_Post $post)
     {
         $sitemap = get_post_meta($post->ID, 'sitemap', true);
         if ($sitemap && intval($sitemap) === 1) {
@@ -35,5 +39,17 @@ class SitemapFilters
         }
 
         return $allowed;
+    }
+
+    public static function removeApiSubdomain(string $permalink)
+    {
+        if (Str::contains($permalink, '://api.')) {
+            return preg_replace('#://api.#', '://', $permalink);
+        } else if (Str::contains($permalink, '://native-api.')) {
+            return preg_replace('#://native-api.#', '://', $permalink);
+        } else if (Str::contains($permalink, '://admin.')) {
+            return preg_replace('#://admin.#', '://', $permalink);
+        }
+        return $permalink;
     }
 }
---
+++ tests/Unit/Actions/Universal/SitemapFiltersTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Bonnier\Willow\Base\Tests\Unit\Actions\Universal;
+
+use Bonnier\Willow\Base\Actions\Universal\SitemapFilters;
+use Bonnier\Willow\Base\Tests\Unit\ClassTestCase;
+
+class SitemapFiltersTest extends ClassTestCase
+{
+
+    public function testAllowedPostTypesRemovesPost()
+    {
+        $postTypes = [
+            'post',
+            'page',
+            'contenthub_composite'
+        ];
+        $this->assertEquals(['page', 'contenthub_composite'], SitemapFilters::allowedPostTypes($postTypes));
+    }
+
+    public function testItRemovesApiSubdomain()
+    {
+        $permalink = 'https://api.example.test/category/subcategory/article-title';
+
+        $this->assertEquals(
+            'https://example.test/category/subcategory/article-title',
+            SitemapFilters::removeApiSubdomain($permalink)
+        );
+    }
+
+    public function testItRemovesNativeApiSubdomain()
+    {
+        $permalink = 'https://native-api.example.test/category/subcategory/article-title';
+
+        $this->assertEquals(
+            'https://example.test/category/subcategory/article-title',
+            SitemapFilters::removeApiSubdomain($permalink)
+        );
+    }
+
+    public function testItRemovesAdminSubdomain()
+    {
+        $permalink = 'https://admin.example.test/category/subcategory/article-title';
+
+        $this->assertEquals(
+            'https://example.test/category/subcategory/article-title',
+            SitemapFilters::removeApiSubdomain($permalink)
+        );
+    }
+
+    public function testItLeavesValidPermalinkUnchanged()
+    {
+        $permalink = 'https://example.test/category/subcategory/api-article-title';
+
+        $this->assertEquals(
+            'https://example.test/category/subcategory/api-article-title',
+            SitemapFilters::removeApiSubdomain($permalink)
+        );
+    }
+}
