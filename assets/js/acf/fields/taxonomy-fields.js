62ab03bea275ad30307129be0153b3bfcc8f9b73
Martin Henriksen <martin.henriksen.92@gmail.com>
2021-01-25 15:04:36 +0100

    Feature: disable other options in select box

--- assets/js/acf/fields/taxonomy-fields.js
+++ assets/js/acf/fields/taxonomy-fields.js
@@ -1,18 +1,21 @@
 (function () {
     function disableElements(selector) {
+        $(selector).attr('readonly', 'readonly');
+    }
+    function disableSelectOptions(selector) {
         document.querySelectorAll(selector).forEach(function (ele){
-            ele.disabled = true;
+            ele.find($('option:not(:selected)').attr('disabled', true));
         });
     }
     document.addEventListener("DOMContentLoaded", function(event) {
         // Disable fields on: Edit Tag
         disableElements('body.taxonomy-post_tag form[name=edittag] table.form-table:first-of-type input');
         disableElements('body.taxonomy-post_tag form[name=edittag] table.form-table:first-of-type textarea');
-        disableElements('body.taxonomy-post_tag form[name=edittag] table.form-table:first-of-type select');
+        disableSelectOptions('body.taxonomy-post_tag form[name=edittag] table.form-table:first-of-type select');
 
         // Disable fields on: Edit Category
         disableElements('body.taxonomy-category form[name=edittag] table.form-table:first-of-type input');
         disableElements('body.taxonomy-category form[name=edittag] table.form-table:first-of-type textarea');
-        disableElements('body.taxonomy-category form[name=edittag] table.form-table:first-of-type select');
+        disableSelectOptions('body.taxonomy-category form[name=edittag] table.form-table:first-of-type select');
     });
 })();
