3638f174db3659fc0294b3feba2d9dd2a7f66fbd
Mads Paaskesen <madspaaskesen@gmail.com>
2021-04-22 13:30:57 +0200

    IFD-283/Problem-with-markdown-in-migrated-texts

--- tests/Unit/Commands/Helpers/ImportHelperTest.php
+++ tests/Unit/Commands/Helpers/ImportHelperTest.php
@@ -15,6 +15,6 @@ class ImportHelperTest extends ClassTestCase
 
         $expectedText = '</h3><p>Alliér dig med en veninde, kollega eller din partner, så vedkommende kan hjælpe dig igennem fx <a href="" class="">ved at hoppe med vognen </a>, <br></br>give dig venlig konkurrence eller bare støtte dig og holde dig op på dit mål.&nbsp; </p><h3> sssskkkks </h3><p>ssssssss</p>';
 
-        $this->assertSame($fixedText, $expectedText);
+        $this->assertEquals($expectedText, $fixedText);
     }
 }
