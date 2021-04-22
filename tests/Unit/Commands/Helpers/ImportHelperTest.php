cf33e14b0b5d2711758525e544467bc10c7e4862
Mads Paaskesen <madspaaskesen@gmail.com>
2021-04-22 13:25:40 +0200

    IFD-283/Problem-with-markdown-in-migrated-texts

--- tests/Unit/Commands/Helpers/ImportHelperTest.php
+++ tests/Unit/Commands/Helpers/ImportHelperTest.php
@@ -9,11 +9,11 @@ class ImportHelperTest extends ClassTestCase
 {
     public function testFixFloatingTextsWithoutParagraphTag()
     {
-        $text = `</h3>Alliér dig med en veninde, kollega eller din partner, så vedkommende kan hjælpe dig igennem fx <a href="" class="">ved at hoppe med vognen </a>, <br></br>give dig venlig konkurrence eller bare støtte dig og holde dig op på dit mål.&nbsp; <h3> sssskkkks </h3>ssssssss<p></p>`;
+        $text = '</h3>Alliér dig med en veninde, kollega eller din partner, så vedkommende kan hjælpe dig igennem fx <a href="" class="">ved at hoppe med vognen </a>, <br></br>give dig venlig konkurrence eller bare støtte dig og holde dig op på dit mål.&nbsp; <h3> sssskkkks </h3>ssssssss<p></p>';
 
         $fixedText = ImportHelper::fixFloatingTextsWithoutParagraphTag($text);
 
-        $expectedText = `</h3><p>Alliér dig med en veninde, kollega eller din partner, så vedkommende kan hjælpe dig igennem fx <a href="" class="">ved at hoppe med vognen </a>, <br></br>give dig venlig konkurrence eller bare støtte dig og holde dig op på dit mål.&nbsp; </p><h3> sssskkkks </h3><p>ssssssss</p>`;
+        $expectedText = '</h3><p>Alliér dig med en veninde, kollega eller din partner, så vedkommende kan hjælpe dig igennem fx <a href="" class="">ved at hoppe med vognen </a>, <br></br>give dig venlig konkurrence eller bare støtte dig og holde dig op på dit mål.&nbsp; </p><h3> sssskkkks </h3><p>ssssssss</p>';
 
         $this->assertSame($fixedText, $expectedText);
     }
