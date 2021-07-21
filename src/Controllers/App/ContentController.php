6d4e88828625d1916248e1f66aa04b2dbaf10933
Mads Paaskesen <madspaaskesen@gmail.com>
2021-07-21 13:44:44 +0200

    IFD-400/Download-af-PDF-virker-ikke-alle-steder
    adding download endpoint of aws files to app/content api controller.

--- src/Controllers/App/ContentController.php
+++ src/Controllers/App/ContentController.php
@@ -7,6 +7,8 @@ use Bonnier\Willow\Base\Helpers\SortBy;
 use Bonnier\Willow\Base\Models\Base\Composites\Composite;
 use Bonnier\Willow\Base\Repositories\WpModelRepository;
 use Bonnier\Willow\Base\Transformers\Api\Composites\CompositeTeaserTransformer;
+use WP_REST_Request;
+use WP_REST_Response;
 
 class ContentController extends BaseController
 {
@@ -16,8 +18,16 @@ class ContentController extends BaseController
             'methods' => \WP_REST_Server::READABLE,
             'callback' => [$this, 'popular']
         ]);
+
+        register_rest_route('app/content', '/download', [
+            'methods' => \WP_REST_Server::READABLE,
+            'callback' => [$this, 'download']
+        ]);
     }
 
+    /**
+     * @return WP_REST_Response
+     */
     public function popular()
     {
         $result = SortBy::getPopularComposites();
@@ -31,5 +41,30 @@ class ContentController extends BaseController
         }
 
         return new \WP_REST_Response(['data' => $composites->toArray()]);
+    }
+
+    /**
+     * @param \WP_REST_Request $request
+     *
+     * @return WP_REST_Response
+     */
+    public function download(WP_REST_Request $request)
+    {
+        $key = $request->get_param('key');
+        $expires = $request->get_param('expires');
+        if (is_numeric($expires)) {
+            $expires = time() + $expires;
+        }
+        else {
+            $expires = time() + HOUR_IN_SECONDS;
+        }
+
+        global $as3cf;
+        $as3Client = $as3cf->get_s3client('eu-west-1');
+        $signedUrl = $as3Client->get_object_url('files.bonnier.cloud', $key, $expires, []);
+
+        return new \WP_REST_Response(['data' => [
+            'signed_url' => $signedUrl,
+        ]]);
     }
 }
