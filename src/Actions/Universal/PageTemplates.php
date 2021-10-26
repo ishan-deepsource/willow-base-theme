b3e9d6c4a0fa7874a727da908774257c73a38616
Martin Henriksen <martin.henriksen.92@gmail.com>
2021-10-26 14:14:46 +0200

    Chore: add last page template

--- src/Actions/Universal/PageTemplates.php
+++ src/Actions/Universal/PageTemplates.php
@@ -90,6 +90,7 @@ class PageTemplates
                     'signup' => 'Signup Page',
                     'login' => 'Login Page',
                     'piano-offer-page' => 'Piano Offer Page',
+                    'author-overview' => 'Author Overview',
                 ]);
                 $this->compositeTemplates = array_merge($this->compositeTemplates, [
                     'guide' => 'Guide',
