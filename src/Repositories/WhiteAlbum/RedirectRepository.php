1c65359a764063b079181701adfb47b7ac4f7369
Martin Henriksen <martin.henriksen.92@gmail.com>
2021-11-02 12:47:55 +0100

    Chore: use https on old domains wa route resolve

--- src/Repositories/WhiteAlbum/RedirectRepository.php
+++ src/Repositories/WhiteAlbum/RedirectRepository.php
@@ -40,7 +40,7 @@ class RedirectRepository
             'allow_redirects' => [
                 'track_redirects' => true,
             ],
-            'base_uri' => sprintf('http://old.%s', $this->domain),
+            'base_uri' => sprintf('https://old.%s', $this->domain),
         ]);
         $this->createRouteResolvesTable();
     }
